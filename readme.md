2b05918385015ca7ec4012a5839b94b70f6f87ea
Andrew Van Slaars <avanslaars@gmail.com>
2017-10-30 15:08:26 -0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Optics Playground
+
+Just a place to tinker with JS optics using the [partial.lenses](https://github.com/calmm-js/partial.lenses) library.
---
+++ wallaby.js
@@ -0,0 +1,9 @@
+module.exports = wallaby => {
+  return {
+    files: ['src/**/*.js', { pattern: 'src/**/*.spec.js', ignore: true }],
+    tests: ['src/**/*.spec.js'],
+    env: {
+      type: 'node'
+    }
+  }
+}
